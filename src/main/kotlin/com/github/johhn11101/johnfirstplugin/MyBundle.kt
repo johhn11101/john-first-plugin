6595123a1710a2b94809dd4b24f114f0d0549e76
GitHub Action <action@github.com>
2022-12-28 20:45:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/johhn11101/johnfirstplugin/MyBundle.kt
+++ src/main/kotlin/com/github/johhn11101/johnfirstplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.johhn11101.johnfirstplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/johhn11101/johnfirstplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/johhn11101/johnfirstplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.johhn11101.johnfirstplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.johhn11101.johnfirstplugin.MyBundle
 
 class MyApplicationService {
 
